b16b03b0dd269dcb32fbf026e8a28e861faa0b40
Francisco José Medrano <66084146+Linceiberico59@users.noreply.github.com>
2020-09-23 17:23:18 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Panel from "./Panel";
+import getWeb3 from "./getWeb3";
+
+export class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        this.web3 = await getWeb3();
+        console.log(this.web3.version);
+    }
+
+    render() {
+        return <React.Fragment>
+            <div className="jumbotron">
+                <h4 className="display-4">Welcome to the Airline!</h4>
+            </div>
+
+            <div className="row">
+                <div className="col-sm">
+                    <Panel title="Balance">
+
+                    </Panel>
+                </div>
+                <div className="col-sm">
+                    <Panel title="Loyalty points - refundable ether">
+
+                    </Panel>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm">
+                    <Panel title="Available flights">
+
+
+                    </Panel>
+                </div>
+                <div className="col-sm">
+                    <Panel title="Your flights">
+
+                    </Panel>
+                </div>
+            </div>
+        </React.Fragment>
+    }
+}
